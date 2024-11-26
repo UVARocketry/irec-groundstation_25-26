10316de026eeb815dac0c01c578c2288c36b3fd3
Christopher Wood <woodchristophermichael@gmail.com>
2024-11-26 13:14:08 -0500

    first commit

---
+++ ui/README.md
@@ -0,0 +1,3 @@
+# Ground Station ui
+
+This will use p5.js (and maybe three.js as well) to draw the rocket and its current state. It will connect via websocket to the server, and then receive data and render it.
---
+++ ui/js/site.js
@@ -0,0 +1,29 @@
+﻿var width, height;
+var light = true;
+
+//Just dont look in here
+const s = (pi) => {
+  p = pi;
+  pi.setup = function () {
+    pi.createCanvas(p.windowWidth, p.windowHeight);
+    width = p.windowWidth;
+    height = p.windowHeight;
+    $("canvas").contextmenu((e) => {
+      e.preventDefault();
+    });
+    p.angleMode(p.DEGREES);
+  };
+
+  pi.draw = function () {
+    if (light) {
+      p.background(255);
+      p.strokeWeight(0);
+    } else {
+      p.fill(255);
+      p.background(4 * 16);
+    }
+  };
+  pi.mouseDragged = function () {};
+  pi.mouseClicked = function () {};
+  pi.mouseReleased = function () {};
+};
