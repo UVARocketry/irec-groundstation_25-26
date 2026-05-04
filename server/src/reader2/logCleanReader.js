608739f2b0f097f48ed4c24d4539c4f188583350
Christopher Wood <woodchristophermichael@gmail.com>
2026-05-04 01:05:39 -0400

    feat: add logCleanReader, also default to using ssl2

--- server/src/reader2/logCleanReader.js
+++ server/src/reader2/logCleanReader.js
@@ -14,7 +14,7 @@ import { Strings } from "../ansi.js";
 
 class Config {
 	/** @type {string} */
-	dir = "../out_ssl2";
+	dir = "../out";
 
 	/** @type {number} */
 	i = 0;
@@ -27,7 +27,7 @@ class Config {
 }
 
 /** @implements {Reader} */
-export class FileLogReader {
+export class FileCleanReader {
 	config = new Configuration(new Config());
 
 	/** @type {(v: Uint8Array<ArrayBuffer>) => Promise<void>}*/
@@ -112,22 +112,28 @@ export class FileLogReader {
 	}
 
 	async determineNewMaxI() {
-		const dir = this.config.get("dir");
-		const files = await fs.promises.readdir(dir);
+		var ceil = 10000;
+		while (await this.saveFileExists(ceil)) {
+			ceil *= 2;
+		}
+		var floor = 1;
 
-		let maxI = -1;
-		for (const file of files) {
-			const match = file.match(/^msg-(\d+)$/);
-			if (match) {
-				const i = parseInt(match[1], 10);
-				if (i > maxI) {
-					maxI = i;
+		while (ceil - 1 >= floor) {
+			var middle = Math.floor((ceil - 1 + floor) / 2);
+			var exists = await this.saveFileExists(middle);
+			if (exists) {
+				if (floor == middle) {
+					break;
 				}
+				floor = middle;
+			} else {
+				if (ceil == middle) {
+					ceil = middle - 1;
+				}
+				ceil = middle;
 			}
 		}
-
-		this.config.setField("maxI", maxI);
-		console.log(`maxI: ${maxI}`);
+		this.config.setField("maxI", floor);
 	}
 
 	async readMessage() {
@@ -146,12 +152,12 @@ export class FileLogReader {
 		}
 		// console.log("maxI: " + this.config.get("maxI"));
 		if (this.config.get("i") > this.config.get("maxI")) {
-			console.log("Done");
 			this.onDone();
 			return;
 		}
 		if (!fs.existsSync(path)) {
 			this.config.setField("i", this.config.get("i") + 1);
+			await this.determineNewMaxI();
 			setTimeout(() => {
 				this.readMessage();
 			}, 1);
@@ -170,12 +176,17 @@ export class FileLogReader {
 			log(`${Strings.Info}: Waiting for ${delta}ms`);
 		}
 
-		setTimeout(
-			() => {
-				this.readMessage();
-			},
-			delta > 20000 ? 0 : delta,
-		);
+		setTimeout(() => {
+			if (delta <= 1 && this.config.get("i") > 3) {
+				console.log(`Removing: ${path}`);
+				fs.rm(path, (a) => {
+					console.log(a);
+				});
+			} else {
+				console.log(`Keeping: ${path}`);
+			}
+			this.readMessage();
+		}, 1);
 	}
 
 	async start() {
