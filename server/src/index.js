68be45b42b4e0559a4558a8a8947afd17e37e05c
Christopher Wood <woodchristophermichael@gmail.com>
2026-06-06 00:47:31 -0500

    server: actually keep useful things

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,7 @@
-// import fs from "node:fs";
+import fs from "node:fs";
 
-// import http from "node:http";
-// import path from "node:path";
+import http from "node:http";
+import path from "node:path";
 
 import { clearSysTime } from "./data.js";
 
@@ -12,7 +12,7 @@ import command from "./command.js";
 import { port } from "../../common/web.js";
 // import { FileLogReader } from "./readers/fileLogReader.js";
 // import { InputReader } from "./readers/inputReader.js";
-// import child_process from "node:child_process";
+import child_process from "node:child_process";
 import { log } from "./log.js";
 // import { SerialPortReader } from "./readers/serialportReader.js";
 // import { StdinReader } from "./readers/stdinReader.js";
@@ -146,109 +146,110 @@ wss.on("connection", function (ws) {
 	log(`${Strings.Ok}: Websocket connection successful`);
 });
 
-// // a server to send off the files
-// const server = http.createServer((req, res) => {
-// 	// If the user requests the root '/'
-// 	const searchDir = process.cwd() + "/../";
-// 	// leave off the common/ because that's in the url
-// 	// const commonDir = process.cwd() + "/../";
-// 	if (req.url === "/") {
-// 		const indexPath = path.join(searchDir, "ui/index.html");
+// a server to send off the files
+const server = http.createServer((req, res) => {
+	// If the user requests the root '/'
+	const searchDir = process.cwd() + "/../";
+	// leave off the common/ because that's in the url
+	// const commonDir = process.cwd() + "/../";
+	if (req.url === "/") {
+		const indexPath = path.join(searchDir, "ui/index.html");
 
-// 		// Serve index.html file
-// 		fs.readFile(indexPath, (err, data) => {
-// 			if (err) {
-// 				res.statusCode = 500;
-// 				res.end("Error loading index.html");
-// 				log(`${Strings.Warn}: Request for ${indexPath} failed`);
-// 			} else {
-// 				log(`${Strings.Ok}: Request for ${indexPath}`);
-// 				res.statusCode = 200;
-// 				res.setHeader("Content-Type", "text/html");
-// 				res.end(data);
-// 			}
-// 		});
-// 	} else {
-// 		// Serve other files from the file system
-// 		var url = req.url ?? "/ui/index.html";
-// 		let filePath = path.join(searchDir, url);
+		// Serve index.html file
+		fs.readFile(indexPath, (err, data) => {
+			if (err) {
+				res.statusCode = 500;
+				res.end("Error loading index.html");
+				log(`${Strings.Warn}: Request for ${indexPath} failed`);
+			} else {
+				log(`${Strings.Ok}: Request for ${indexPath}`);
+				res.statusCode = 200;
+				res.setHeader("Content-Type", "text/html");
+				res.end(data);
+			}
+		});
+	} else {
+		// Serve other files from the file system
+		var url = req.url ?? "/ui/index.html";
+		let filePath = path.join(searchDir, url);
 
-// 		// if (url.startsWith("/common") || url.startsWith("common")) {
-// 		// 	filePath = path.join(commonDir, url);
-// 		// }
+		// if (url.startsWith("/common") || url.startsWith("common")) {
+		// 	filePath = path.join(commonDir, url);
+		// }
 
-// 		try {
-// 			if (fs.statSync(filePath).isDirectory()) {
-// 				if (!filePath.endsWith("/")) {
-// 					filePath += "/";
-// 				}
-// 				filePath += "index.html";
-// 			}
-// 		} catch (_) {}
-// 		const prettyPath = filePath.replace(process.cwd(), "");
+		try {
+			if (fs.statSync(filePath).isDirectory()) {
+				if (!filePath.endsWith("/")) {
+					filePath += "/";
+				}
+				filePath += "index.html";
+			}
+		} catch (_) {}
+		const prettyPath = filePath.replace(process.cwd(), "");
 
-// 		// Check if file exists
-// 		fs.exists(filePath, (exists) => {
-// 			if (exists) {
-// 				fs.readFile(filePath, (err, data) => {
-// 					if (err) {
-// 						res.statusCode = 500;
-// 						res.end("Error reading the file");
-// 						log(
-// 							`${Strings.Warn}: Request for ${prettyPath} failed`,
-// 						);
-// 					} else {
-// 						// Guess the content type based on file extension
-// 						let contentType = "text/plain";
-// 						if (filePath.endsWith(".html")) {
-// 							contentType = "text/html";
-// 						} else if (filePath.endsWith(".css")) {
-// 							contentType = "text/css";
-// 						} else if (filePath.endsWith(".js")) {
-// 							contentType = "application/javascript";
-// 						}
+		// Check if file exists
+		fs.exists(filePath, (exists) => {
+			if (exists) {
+				fs.readFile(filePath, (err, data) => {
+					if (err) {
+						res.statusCode = 500;
+						res.end("Error reading the file");
+						log(
+							`${Strings.Warn}: Request for ${prettyPath} failed`,
+						);
+					} else {
+						// Guess the content type based on file extension
+						let contentType = "text/plain";
+						if (filePath.endsWith(".html")) {
+							contentType = "text/html";
+						} else if (filePath.endsWith(".css")) {
+							contentType = "text/css";
+						} else if (filePath.endsWith(".js")) {
+							contentType = "application/javascript";
+						}
 
-// 						res.statusCode = 200;
-// 						res.setHeader("Content-Type", contentType);
-// 						res.end(data);
-// 						log(`${Strings.Ok}: Request for ${prettyPath}`);
-// 					}
-// 				});
-// 			} else {
-// 				log(`${Strings.Error}: Request for ${prettyPath} failed`);
-// 				res.statusCode = 404;
-// 				res.end("File not found");
-// 			}
-// 		});
-// 	}
-// });
+						res.statusCode = 200;
+						res.setHeader("Content-Type", contentType);
+						res.end(data);
+						log(`${Strings.Ok}: Request for ${prettyPath}`);
+					}
+				});
+			} else {
+				log(`${Strings.Error}: Request for ${prettyPath} failed`);
+				res.statusCode = 404;
+				res.end("File not found");
+			}
+		});
+	}
+});
 
-// /**
-//  * @param {string} url
-//  */
-// function openUrl(url) {
-// 	var start =
-// 		process.platform == "darwin"
-// 			? "open"
-// 			: process.platform == "win32"
-// 				? "start"
-// 				: "xdg-open";
-// 	if (process.platform === "win32") {
-// 		url = url.replaceAll("&", "^&");
-// 	}
-// 	child_process.exec(start + " " + url);
-// }
+/**
+ * @param {string} url
+ */
+function openUrl(url) {
+	var start =
+		process.platform == "darwin"
+			? "open"
+			: process.platform == "win32"
+				? "start"
+				: "xdg-open";
+	if (process.platform === "win32") {
+		url = url.replaceAll("&", "^&");
+	}
+	child_process.exec(start + " " + url);
+}
 
-// // Set the server to listen on port 3000
-// const PORT = 3000;
-// server.listen(PORT, () => {
-// 	log(`${Strings.Ok}: Server running at http://localhost:${PORT}`);
-// 	var url = "http://localhost:" + PORT;
-// 	if (argv.indexOf("--nowin") !== 0) {
-// 		openUrl(url);
-// 		openUrl(url + "/uiforconfig");
-// 	}
-// });
+// Set the server to listen on port 3000
+const PORT = 3000;
+server.listen(PORT, () => {
+	log(`${Strings.Ok}: Server running at http://localhost:${PORT}`);
+	var url = "http://localhost:" + PORT;
+	if (argv.indexOf("--nowin") !== 0) {
+		openUrl(url);
+		openUrl(url + "/uiforconfig");
+	}
+});
 
 log(`${Strings.Ok}: Data pipe active on ws://localhost:${port}`);
 log(`${Strings.Ok}: New Ground Station ready for connection.`);
+
